ee1273363295b13893f41a41d3a4397d3dc07000
Sven Ruppert <sven.ruppert@gmail.com>
2019-06-14 09:02:42 +0200

    added docker_run_locale.sh

---
+++ docker_run_locale.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run \
+       -it \
+       -p 8899:8899 \
+       --rm \
+       --name run \
+       -v "$(pwd)":/usr/src/mymaven \
+       -w /usr/src/mymaven \
+       svenruppert/adopt:1.8.212-04 \
+       java -jar target/vaadin-app.jar
+
+#docker run -it -p 8080:8080 -p 5005:5005 --rm --name run -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven svenruppert/maven-3.5-jdk-oracle-08 mvn meecrowave:bake -DargLine="-Xmx256m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
