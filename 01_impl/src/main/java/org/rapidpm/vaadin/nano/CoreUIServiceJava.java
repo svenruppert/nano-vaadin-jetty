3fca96f618f09bc8dc28b7c24e835155fd043cab
Sven Ruppert <sven.ruppert@gmail.com>
2019-10-02 17:20:35 +0200

    want to get merge conflict with change on feature branch

--- 01_impl/src/main/java/org/rapidpm/vaadin/nano/CoreUIServiceJava.java
+++ 01_impl/src/main/java/org/rapidpm/vaadin/nano/CoreUIServiceJava.java
@@ -52,10 +52,10 @@ public class CoreUIServiceJava
   public static final String CLI_PORT = "port";
 
   public static void main(String[] args) throws ParseException {
-    new CoreUIServiceJava().executeCLI_NEW(args).startup();
+    new CoreUIServiceJava().executeCLINEW(args).startup();
   }
 
-  public CoreUIServiceJava executeCLI_NEW(String[] args) throws ParseException {
+  public CoreUIServiceJava executeCLINEW(String[] args) throws ParseException {
     final Options options = new Options();
     options.addOption(CLI_HOST, true, "host to use");
     options.addOption(CLI_PORT, true, "port to use");
